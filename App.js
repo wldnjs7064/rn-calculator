a299019e6d837b1914b658e3a1a922165673b2b8
wldnjs7064 <wldnjs7064@naver.com>
2023-08-23 15:39:35 +0900

    환경 세팅

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const name = "jiwon";
+  const add = (a, b) => a + b;
+  const isFullName = true;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text>1+2={add(1, 2)}</Text>
+      <Text>{isFullName === true ? name + "Song" : name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
